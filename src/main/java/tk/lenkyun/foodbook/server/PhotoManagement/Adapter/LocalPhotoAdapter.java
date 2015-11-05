0559bf8cdf2e05bef7a862fab8a6256a8ea8e0d0
Rapeapach Suwasri <kagaminelenkyun@gmail.com>
2015-11-05 23:28:28 +0700

    Initial

--- src/main/java/tk/lenkyun/foodbook/server/PhotoManagement/Adapter/LocalPhotoAdapter.java
+++ src/main/java/tk/lenkyun/foodbook/server/PhotoManagement/Adapter/LocalPhotoAdapter.java
@@ -3,8 +3,10 @@ package tk.lenkyun.foodbook.server.PhotoManagement.Adapter;
 import org.springframework.stereotype.Repository;
 import tk.lenkyun.foodbook.foodbook.Domain.Data.Photo.PhotoContent;
 
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.UUID;
 
 /**
  * Created by lenkyun on 5/11/2558.
@@ -13,7 +15,29 @@ import java.net.URI;
 public class LocalPhotoAdapter implements PhotoAdapter {
     @Override
     public URI postPhoto(PhotoContent photoContent) {
-        PrintWriter writer = new PrintWriter("./");
+        UUID uuid = UUID.randomUUID();
+        File dir = new File("./dir");
+        if(!dir.exists()){
+            dir.mkdir();
+        }
+
+        File file = new File("./img/" + uuid.toString() + ".jpg");
+        while(file.exists()){
+            uuid = UUID.randomUUID();
+            file = new File("./img/" + uuid.toString() + ".jpg");
+        };
+
+        try {
+            FileOutputStream writer = new FileOutputStream("./img/" + uuid.toString() + ".jpg");
+            writer.write(photoContent.getContent());
+            writer.flush();
+            writer.close();
+
+            return new URI("http://192.168.56.1/img/" + uuid.toString() + ".jpg");
+        }
+        catch (FileNotFoundException e) {}
+        catch (IOException e) {}
+        catch (URISyntaxException e) {}
         return null;
     }
 
